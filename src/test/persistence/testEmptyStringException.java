bc85dd3e74445d61ace7dbe25f278753f3d72ff1
nazish <nazish@students.cs.ubc.ca>
2020-08-12 15:36:31 -0700

    phase 3

---
+++ src/test/persistence/testEmptyStringException.java
@@ -0,0 +1,34 @@
+package persistence;
+
+
+import exceptions.EmptyStringException;
+import model.Film;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class testEmptyStringException {
+    private Film f1;
+
+    @Test
+    void testNonEmptyString() {
+        try {
+            f1 = new Film("Jules et Jim",1962,"Francois Truffaut",
+                    "Criterion Collection",7.8);
+        } catch (EmptyStringException e) {
+            fail("Empty String Exception should not have been thrown");
+        }
+    }
+
+    @Test
+    void testEmptyString() {
+        try {
+            f1 = new Film("",1962,"",
+                    "Criterion Collection",7.8);
+            fail();
+        } catch (EmptyStringException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
